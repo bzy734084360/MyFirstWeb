95f2672e407dd3ccd5158cb9e57c4564d87bb2b4
bzy734084360 <bzy734084360@hotmail.com>
2021-02-24 18:09:51 +0800

    MVC5 学习

---
+++ MVC5/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Store.Index()";
+        }
+        //genre 响应URL传参
+        public string Browse(string genre)
+        {
+            //防止JS或html 标记注入
+            string message = HttpUtility.HtmlEncode($"Store.Browse(),Genre={genre}");
+            return message;
+        }
+
+        public string Details(int id)
+        {
+            string message = $"Store.Details(),ID={id}";
+            return message;
+        }
+
+        public ActionResult XSS()
+        {
+            return View();
+        }
+
+    }
+}
