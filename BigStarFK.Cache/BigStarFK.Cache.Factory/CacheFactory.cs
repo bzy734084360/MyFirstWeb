d1c92bfea52cc22cb92846f512d862f5116b17f1
fu.yu <grumpyfish@DESKTOP-RIRJP7K>
2021-07-19 11:22:46 +0800

    增加redishelper

---
+++ BigStarFK.Cache/BigStarFK.Cache.Factory/CacheFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+
+namespace BigStarFK.Cache.Factory
+{
+    /// <summary>
+    /// 缓存工厂类
+    /// </summary>
+    public class CacheFactory
+    {
+        public static ICache Cache()
+        {
+            //string cacheType = ConfigurationManager.AppSettings["CacheType"].Trim();
+            string cacheType = "Redis";
+            switch (cacheType)
+            {
+                case "Redis":
+                    return new Redis.StackExchangeRedisCache();
+                //return new Redis.Cache();
+                case "WebCache":
+                    return new Cache();
+                default:
+                    return new Cache();
+            }
+        }
+    }
+}
