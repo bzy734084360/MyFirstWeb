516121545cf4042e93706a8b7e7e600afebe4e7b
fu.yu <grumpyfish@DESKTOP-RIRJP7K>
2021-08-23 10:22:31 +0800

    OAuth2.0项目结构修改

---
+++ MyWebAPI/App_Start/Startup.Auth.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using MyWebAPI.Auth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebAPI
+{
+    public partial class Startup
+    {
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,//允许客户端一http协议请求,在生产模式下设 AllowInsecureHttp = false
+                TokenEndpointPath = new PathString("/token"),//token请求的地址，即http://localhost:端口号/token；
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),//token过期时间；
+                Provider = new SimpleAuthorizationServerProvider(),//提供具体的认证策略;
+                                                                   //刷新token
+                RefreshTokenProvider = new SimpleRefreshTokenProvider()
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
--- MyWebAPI/Startup.cs
+++ MyWebAPI/Startup.cs
@@ -11,7 +11,7 @@ using System.Web.Http;
 [assembly: OwinStartup(typeof(MyWebAPI.Startup))]
 namespace MyWebAPI
 {
-    public class Startup
+    public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
@@ -23,21 +23,5 @@ namespace MyWebAPI
             app.UseWebApi(config);
         }
 
-        public void ConfigureOAuth(IAppBuilder app)
-        {
-            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
-            {
-                AllowInsecureHttp = true,//允许客户端一http协议请求,在生产模式下设 AllowInsecureHttp = false
-                TokenEndpointPath = new PathString("/token"),//token请求的地址，即http://localhost:端口号/token；
-                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(30),//token过期时间；
-                Provider = new SimpleAuthorizationServerProvider(),//提供具体的认证策略;
-                //刷新token
-                RefreshTokenProvider = new SimpleRefreshTokenProvider()
-            };
-
-            app.UseOAuthAuthorizationServer(OAuthServerOptions);
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
-
-        }
     }
 }
